232f691d82cda3a86191daccc0e7ffc0e2bc2c6f
Richard Westerhof <38462668+richardswesterhof@users.noreply.github.com>
2021-02-18 17:28:10 +0100

    going to normalise line endings

--- mapreduce/main.py
+++ mapreduce/main.py
@@ -1,5 +1,5 @@
 from random import random
-
+from pyspark.sql import SparkSession
 import pyspark
 
 spark_conf = pyspark.SparkConf()
@@ -17,17 +17,54 @@ spark_conf.setAll([
     ('spark.kubernetes.driver.pod.name', 'spark-driver'),
     ('spark.driver.port', '30001'),
     ('spark.driver.blockManager.port', '30002'),
+    # add this to communicate with elastic
+    ('spark.jars', './spark-jars/elasticsearch-hadoop-7.11.1.jar'),
 ])
 
 sc = pyspark.SparkContext(conf=spark_conf).getOrCreate()
-NUM_SAMPLES = 10000
+
+# spark = SparkSession.builder.config(spark_conf).getOrCreate()
 
 
-def inside(p):
-    x, y = random(), random()
-    return x * x + y * y < 1
+q = """{
+    "query": {
+        "bool" : {
+            "must" : {
+                "match_all" : {}
+            },
+           "filter" : {
+                "user_id": "16217679"
+            }
+        }
+    }
+}"""
+
+# NUM_SAMPLES = 10000
+#
+#
+# def inside(p):
+#     x, y = random(), random()
+#     return x * x + y * y < 1
+#
+#
+# count = sc.parallelize(range(0, NUM_SAMPLES)) \
+#     .filter(inside).count()
+# print("Pi is roughly %f" % (4.0 * count / NUM_SAMPLES))
+
+es_read_conf = {
+    "es.nodes" : "elasticsearch.es-raw",
+    "es.port" : "9200",
+    "es.index" : "tweets",
+    "es.query" : q
+}
+
+es_rdd = sc.newAPIHadoopRDD(
+    inputFormatClass="org.elasticsearch.hadoop.mr.EsInputFormat",
+    keyClass="org.apache.hadoop.io.NullWritable",
+    valueClass="org.elasticsearch.hadoop.mr.LinkedMapWritable",
+    conf=es_read_conf)
 
 
-count = sc.parallelize(range(0, NUM_SAMPLES)) \
-    .filter(inside).count()
-print("Pi is roughly %f" % (4.0 * count / NUM_SAMPLES))
+print(es_rdd.first())
+
+# print(tweets)
