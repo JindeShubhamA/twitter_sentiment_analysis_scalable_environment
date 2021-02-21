43f92241e5192db94c6f3923838181c7d602ee28
Richard Westerhof <38462668+richardswesterhof@users.noreply.github.com>
2021-02-21 22:15:17 +0100

    removed namespacing

--- mapreduce/src/main.py
+++ mapreduce/src/main.py
@@ -1,3 +1,4 @@
+import json
 import time
 from random import random
 from pyspark.sql import SparkSession
@@ -65,21 +66,63 @@ q = """{
 #     .filter(inside).count()
 # print("Pi is roughly %f" % (4.0 * count / NUM_SAMPLES))
 
-es_read_conf = {
-    "es.nodes" : "elasticsearch-raw",
-    "es.port" : "9200",
-    "es.resource" : "tweets/_doc",
-    "es.query" : q
-}
+# es_read_conf = {
+#     "es.nodes" : "elasticsearch-raw",
+#     "es.port" : "9200",
+#     "es.resource" : "tweets/_doc",
+#     "es.query" : q
+# }
 
-es_rdd = sc.newAPIHadoopRDD(
-    inputFormatClass="org.elasticsearch.hadoop.mr.EsInputFormat",
+# es_rdd = sc.newAPIHadoopRDD(
+#     inputFormatClass="org.elasticsearch.hadoop.mr.EsInputFormat",
+#     keyClass="org.apache.hadoop.io.NullWritable",
+#     valueClass="org.elasticsearch.hadoop.mr.LinkedMapWritable",
+#     conf=es_read_conf)
+#
+#
+# print("output below:")
+# print(es_rdd.first())
+
+# es_rdd = sc.parallelize([{'num': i} for i in range(10)])
+
+# python_rdd = es_rdd.map(lambda item: ('key', {
+#     'id': item['num'],
+# }))
+#
+# es_write_conf = {
+#     "es.nodes" : "elasticsearch-proc",
+#     "es.port" : "9200",
+#     "es.resource" : "tweets/_doc",
+#     # "es.input.json": "yes",
+#     # "es.mapping.id": "doc_id"
+# }
+
+# python_rdd.saveAsNewAPIHadoopFile(
+#     path='-',
+#     outputFormatClass="org.elasticsearch.hadoop.mr.EsOutputFormat",
+#     keyClass="org.apache.hadoop.io.NullWritable",
+#     valueClass="org.elasticsearch.hadoop.mr.LinkedMapWritable",
+#     conf=es_write_conf)
+
+rdd = sc.parallelize([{'num': i} for i in range(10)])
+def remove__id(doc):
+    # `_id` field needs to be removed from the document
+    # to be indexed, else configure this in `conf` while
+    # calling the `saveAsNewAPIHadoopFile` API
+    doc.pop('_id', '')
+    return doc
+new_rdd = rdd.map(remove__id).map(json.dumps).map(lambda x: ('key', x))
+new_rdd.saveAsNewAPIHadoopFile(
+    path='-',
+    outputFormatClass="org.elasticsearch.hadoop.mr.EsOutputFormat",
     keyClass="org.apache.hadoop.io.NullWritable",
     valueClass="org.elasticsearch.hadoop.mr.LinkedMapWritable",
-    conf=es_read_conf)
-
-
-print("output below:")
-print(es_rdd)
+    conf={
+        "es.nodes" : 'elasticsearch-proc',
+        "es.port" : '9200',
+        "es.resource" : '%s/%s' % ('index_name', 'doc_type_name'),
+        "es.input.json": 'true'
+    }
+)
 
 # print(tweets)
