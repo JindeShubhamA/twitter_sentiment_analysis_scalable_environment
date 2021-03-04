da1cfda0d9bf82a85effd399fb313949a61a1637
Radu Catarambol <catarambolradu@gmail.com>
2021-03-04 17:58:39 +0200

    Parallelized populating-db

--- elasticsearch/python_code/populating_db/import_in_elasticsearch.py
+++ elasticsearch/python_code/populating_db/import_in_elasticsearch.py
@@ -1,5 +1,6 @@
 from elasticsearch import Elasticsearch
-from elasticsearch.helpers import bulk
+from elasticsearch.helpers import bulk, parallel_bulk
+from collections import deque #needed for parallel_bulk
 import csv
 
 es = Elasticsearch(hosts=['elasticsearch:9200'])
@@ -52,6 +53,7 @@ print('Done!')
 print('Populating database...')
 with open('tweet_data_500k.csv', newline='\n') as f:
     reader = csv.DictReader(f, delimiter='\t')
-    bulk(es, reader, index='tweets', raise_on_error=False) # skips rows with bad timestamps
+    # bulk(es, reader, index='tweets', raise_on_error=False) # skips rows with bad timestamps
+    deque(parallel_bulk(es, reader, index='tweets', raise_on_error=False, thread_count=6), maxlen=0)
 
 print('Done!')
