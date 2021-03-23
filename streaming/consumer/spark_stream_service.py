c571251c14b947261640bf295cf39ff0d69042ce
JindeShubhamA <s.jinde@student.rug.nl>
2021-03-24 00:09:39 +0100

    bug fix on mapping location

--- streaming/consumer/spark_stream_service.py
+++ streaming/consumer/spark_stream_service.py
@@ -1,5 +1,5 @@
 from pyspark.sql.functions import (
-    from_json, col, window, avg, desc, max, min, udf)
+    from_json, col, window, avg, desc, max, min, udf, sum)
 
 # import numpy as np
 from spark_base import Source
@@ -21,9 +21,6 @@ from shapely.geometry import shape, Point
 from random import randrange
 
 
-
-
-
 def find_by_string(coord_string) :
     coords = coord_string.split(",")
     return  float(coords[0]), float(coords[1])
@@ -39,27 +36,25 @@ def get_tweet_sentiment(tweet):
 
 
 def get_location(lat_lon):
-    # shp = open("scripts/consumer/shapefiles/us_states.shp", "rb")
-    # dbf = open("scripts/consumer/shapefiles/us_states.dbf", "rb")
-    # shp_reader = shapefile.Reader(shp=shp, dbf=dbf)
-    # all_shapes = shp_reader.shapes()  # get all the polygons
-    # all_records = shp_reader.records()
-    # j = 0
-    #
-    # for i in range(len(all_shapes)):
-    #     boundary = all_shapes[i]  # get a boundary polygon
-    #     shape_bndry = shape(boundary)
-    #     point = Point(find_by_string(lat_lon))
-    #     if point.within(shape_bndry):
-    #         j = 1
-    return "CALIFORNIA"
-    # if not j:
-    #     k = randrange(49)
-    #     return all_records[k][3]
-    # else:
-    #     return all_records[j][3]
+    shp = open("/scripts/consumer/us_states.shp", "rb")
+    dbf = open("/scripts/consumer/us_states.dbf", "rb")
+    shp_reader = shapefile.Reader(shp=shp, dbf=dbf)
+    all_shapes = shp_reader.shapes()  # get all the polygons
+    all_records = shp_reader.records()
+    flag = 0
 
-
+    for i in range(len(all_shapes)):
+        boundary = all_shapes[i]  # get a boundary polygon
+        shape_bndry = shape(boundary)
+        point = Point(find_by_string(lat_lon))
+        if point.within(shape_bndry):
+            flag = 1
+            break
+    if not flag:
+        k = randrange(49)
+        return all_records[k][3]
+    else:
+        return all_records[i][3]
 
 def connect_to_kafka_stream(topic_name, spark_session):
     return (spark_session
@@ -91,11 +86,11 @@ if __name__ == "__main__":
 
     sentiment_udf = udf(lambda z: get_tweet_sentiment(z), StringType())
 
-    #df =df.selectExpr("CAST(value AS STRING) AS json").select(from_json(col("json"), schema).alias("data")).select("data.tweet",sentiment_udf('data.tweet').alias('sentiment'))
+    #df1 =df.selectExpr("CAST(value AS STRING) AS json").select(from_json(col("json"), schema).alias("data")).select("data.tweet",sentiment_udf('data.tweet').alias('sentiment'))
 
     df = df.selectExpr("CAST(value AS STRING) AS json").select(from_json(col("json"), schema).alias("data")).select("data.location",get_state_udf('data.location').alias('state'),"data.tweet",sentiment_udf('data.tweet').alias('sentiment'))
 
-
+    #df2 = df.selec("data.*").groupBy(col("state")).agg(avg("price"))
 
     query = (df
              .writeStream
@@ -104,5 +99,12 @@ if __name__ == "__main__":
              .format("console")
              .start()
              )
-    # query1.awaitTermination()
+    # query1= (df1
+    #          .writeStream
+    #          .option("truncate", "false")
+    #          .outputMode("append")
+    #          .format("console")
+    #          .start()
+    #          )
+    #query1.awaitTermination()
     query.awaitTermination()
